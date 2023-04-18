b6261560d04ec64f08e80038881fc90d65cfd796
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-18 19:44:33 +0530

    Added reverse scroll animation

--- src/js/scrollanimate-reverse.js
+++ src/js/scrollanimate-reverse.js
@@ -2,13 +2,13 @@ const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("slide-out");
+        entry.target.classList.add("slide-out-reverse");
       } else {
-        entry.target.classList.remove("slide-out");
+        entry.target.classList.remove("slide-out-reverse");
       }
     });
   },
   { threshold: 0.5 }
 );
-const hiddenElements = document.querySelectorAll(".slide-in");
+const hiddenElements = document.querySelectorAll(".slide-in-reverse");
 hiddenElements.forEach((element) => observer.observe(element));
